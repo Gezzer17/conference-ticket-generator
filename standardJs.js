5210788bcdd8422424335e5885f6938cff282c7b
Gezzer17 <topalbegovicd@gmail.com>
2025-02-10 15:33:56 +0100

    Nastiman error za Upload preko 500KB, nastimana validacija za input slike koja nije u formatu jpeg,ili png

--- standardJs.js
+++ standardJs.js
@@ -16,9 +16,44 @@ divUpload.addEventListener('click',()=>
 
 let provjeraTipaFajla = (string)=>
 {
-        if(string !== 'image/jpeg' || string !== 'image/png')
+        console.log(string);
+        console.log(typeof string);
+        
+        if(string === 'image/jpeg' || string === 'image/png')
         {
-            console.log("Slika nije u ispravnom formatu!");
+            
+            
+            return true;
+
+        }
+        
+
+        return false;
+}
+
+let provjeraVelicineFajla = (velicinaFajla)=>
+{
+
+        let vrijednostKB = velicinaFajla / 1000;
+        if(vrijednostKB < 500)
+        {
+           
+            console.log(vrijednostKB);
+        }
+        else
+        {
+            
+            console.log(vrijednostKB);
+            const pLabelaIspod = document.querySelector('.ispod-info-label p');
+            let stalniTekst = pLabelaIspod.innerHTML;
+            pLabelaIspod.innerHTML = "File too large. Please upload photo under 500KB";
+            pLabelaIspod.classList.toggle('state-error');
+            setTimeout(() => {
+                
+                pLabelaIspod.innerHTML = `${stalniTekst}`;
+                pLabelaIspod.classList.toggle('state-error');
+
+            }, 5000);
         }
 
 
@@ -32,15 +67,19 @@ let provjeraTipaFajla = (string)=>
 inputUpload.addEventListener('change',(e)=>
     {
         let fajlSLike = e.target.files[0];
+
         if(fajlSLike)
         {
-            
+            if(!provjeraTipaFajla(fajlSLike.type) || !provjeraVelicineFajla(fajlSLike.size))
+            {
+                return;
+            }
 
             
             let reader = new FileReader();
             reader.onload = function(e)
             {
-                
+
 
             }
             reader.readAsDataURL(fajlSLike);
